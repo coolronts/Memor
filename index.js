30a7f1ab60c9c0aba83d42d46de5fb084284912f
Zahid Ronty <58170502+coolronts@users.noreply.github.com>
2021-07-09 12:11:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-const path = require("path");
-const serveStatic = require("serve-static");
 
 //Import Routes
 const routesMiddleware = require("./routes/index");
@@ -12,6 +10,7 @@ const routesMiddleware = require("./routes/index");
 const Port = process.env.PORT || 5000;
 
 const connString = process.env.DB_CONNECTION;
+
 //Database Connection
 mongoose
     .connect(connString, {
@@ -20,7 +19,7 @@ mongoose
         useUnifiedTopology: true,
     })
     .then((result) => {
-        app.listen(Port, () => {
+        server = app.listen(Port, () => {
             console.log("Server started on", Port);
         });
     })
@@ -32,12 +31,3 @@ app.use(cors());
 
 //Route Middleware
 app.use("/api", routesMiddleware);
-
-//Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/dist"));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
-    });
-}
